1256046985552b0044fd9a296d09c4ca51717b52
andrewpjlewis <lewiand1234@gmail.com>
2025-06-04 14:41:43 -0600

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -57,7 +57,6 @@ function Footer({ onAddClick }) {
                     <path fill="currentColor" d="m9.25 22l-.4-3.2q-.325-.125-.612-.3t-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.338v-.675q0-.163.025-.338L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375t.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3t.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.338v.674q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375t-.6.3l-.4 3.2zm2.8-6.5q1.45 0 2.475-1.025T15.55 12t-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12t1.013 2.475T12.05 15.5"/>
                 </svg>
 
-<<<<<<< HEAD
                 {/* Settings panel appears here */}
                 {showSettings && (
                 <div ref={settingsRef} className="settings-panel">
@@ -65,13 +64,6 @@ function Footer({ onAddClick }) {
                     <button onClick={handleLogout}>Logout</button>
                 </div>
                 )}
-=======
-            {/* Settings panel appears here */}
-            {showSettings && (
-            <div ref={settingsRef} className="settings-panel">
-                {/* <button onClick={onHelp}>Help</button> */}
-                <button id="logout" onClick={handleLogout}>Logout</button>
->>>>>>> 2cf4a38144890d948d5ade12a1821cd8e35fe834
             </div>
         </footer>
     );
