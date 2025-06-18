695be86d8c9ae0190cf9e985f2c70911e269e01b
apjgaming <lewiand1234@gmail.com>
2025-06-18 11:39:07 -0600

    user model update

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true }, // keep unique here
+  firstname: { type: String, required: true },  // added first name
+  lastname: { type: String, required: true },   // added last name
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true }); // adds createdAt and updatedAt automatically
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
 
 module.exports = mongoose.model('User', userSchema);
