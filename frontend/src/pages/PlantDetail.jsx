92718dbfa1ec6f5181dc6b86cf2ba6b9c7fd2481
PineappleLord2020 <mul20002@byui.edu>
2025-06-18 13:57:06 -0600

    Updates to styles and screen sizes

--- frontend/src/pages/PlantDetail.jsx
+++ frontend/src/pages/PlantDetail.jsx
@@ -90,35 +90,36 @@ function PlantDetail() {
               alt={`Plant image for ${habit.name}`}
               className="plant-image"
             />
-            <p>Progress: {habit.progress} / {habit.frequency}</p>
-            <p>Frequency: {habit.frequency}x per week</p>
-
-            <button
-              className="delete-button"
-              onClick={handleDelete}
-            >
-              Delete Habit
-            </button>
-
-            <button
-              className="watering-can-button"
-              onClick={handleComplete}
-              disabled={isMaxed}
-              title={isMaxed ? "Max progress reached" : "Water plant"}
-            >
-              {/* Inline SVG for watering can */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                className={`watering-can-icon ${isMaxed ? 'disabled' : ''}`}
+            <p class="progressBar">Progress: {habit.progress} / {habit.frequency}</p>
+            <p class="frequency">Frequency: {habit.frequency}x per week</p>
+            <div class="buttonGroup">
+              <button
+                className="delete-button"
+                onClick={handleDelete}
               >
-                <path
-                  fill={isMaxed ? '#aaa' : '#3498db'}
-                  d="M19 5c-.6 0-1 .4-1 1v2.1l-3.2-.6c-.3-.1-.5.1-.6.3l-.8 2.1-2-1.1V8c0-.6-.4-1-1-1s-1 .4-1 1v1.5L6 10V8c0-.6-.4-1-1-1s-1 .4-1 1v2.3c0 .5.3.8.7.9l5.3 1.2-1.7 5.6c-.2.6.2 1.3.9 1.4.6.2 1.3-.2 1.4-.9l1.8-6.1 2.6 1.4c.5.3 1.1.1 1.4-.4l1.1-2.9 2.1.4V18c0 .6.4 1 1 1s1-.4 1-1V6c0-.6-.4-1-1-1z"
-                />
-              </svg>
-              {isMaxed ? 'Done' : 'Water Plant'}
-            </button>
+              Delete Habit
+              </button>
+
+              <button
+                className="watering-can-button"
+                onClick={handleComplete}
+                disabled={isMaxed}
+                title={isMaxed ? "Max progress reached" : "Water plant"}
+              >
+                {/* Inline SVG for watering can */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className={`watering-can-icon ${isMaxed ? 'disabled' : ''}`}
+                >
+                  <path
+                    fill={isMaxed ? '#aaa' : '#3498db'}
+                    d="M19 5c-.6 0-1 .4-1 1v2.1l-3.2-.6c-.3-.1-.5.1-.6.3l-.8 2.1-2-1.1V8c0-.6-.4-1-1-1s-1 .4-1 1v1.5L6 10V8c0-.6-.4-1-1-1s-1 .4-1 1v2.3c0 .5.3.8.7.9l5.3 1.2-1.7 5.6c-.2.6.2 1.3.9 1.4.6.2 1.3-.2 1.4-.9l1.8-6.1 2.6 1.4c.5.3 1.1.1 1.4-.4l1.1-2.9 2.1.4V18c0 .6.4 1 1 1s1-.4 1-1V6c0-.6-.4-1-1-1z"
+                  />
+                </svg>
+                {isMaxed ? 'Done' : 'Water Plant'}
+              </button>
+            </div>
           </div>
         </div>
       </main>
